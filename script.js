779a1f7b4f812be99cef2644925400537415f9a3
atjrzzz <77987423+atjrzzz@users.noreply.github.com>
2025-02-13 15:21:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // script.js
+const sidebar = document.getElementById('sidebar');
+const sidebarToggle = document.getElementById('sidebar-toggle');
+const mainContent = document.querySelector('main');
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
+// Sidebar Toggle
+sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    mainContent.classList.toggle('shift');
+});
+
+// Dark Mode Toggle
 themeToggle.addEventListener('click', () => {
     body.dataset.theme = body.dataset.theme === 'dark' ? 'light' : 'dark';
     themeToggle.textContent = body.dataset.theme === 'dark' ? 'Toggle Light Mode' : 'Toggle Dark Mode';
