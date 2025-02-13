64620fa61ccb3f639b2424eee18c0496d55da194
atjrzzz <77987423+atjrzzz@users.noreply.github.com>
2025-02-13 14:12:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.dataset.theme = body.dataset.theme === 'dark' ? 'light' : 'dark';
+    themeToggle.textContent = body.dataset.theme === 'dark' ? 'Toggle Light Mode' : 'Toggle Dark Mode';
+});
