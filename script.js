69c56e4a2c4f6f4e7d4cc15a4b19ef3a70251fd9
Jerson jemenez <atjrzzz@gmail.com>
2025-03-04 16:04:19 +0800

    styles and script

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function closeNav() {
     sideNav.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+}
